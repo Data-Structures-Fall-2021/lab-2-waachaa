7a2e637fc34f088c4883b8dc2273e408c404f38c
waachaa <58870161+waachaa@users.noreply.github.com>
2021-09-26 17:49:52 -0400

    Update Lab02.java

--- src/main/java/Lab02.java
+++ src/main/java/Lab02.java
@@ -1,5 +1,9 @@
 public class Lab02 {
-
-	// YOUR CODE GOES HERE
-
+	public static double[] map(UnaryFunction unary, double[] arr) {
+        double[] arr2 = new double[arr.length];
+        for (int i=0; i<arr.length; i++) {
+            arr2[i] += unary.exec(arr[i]);
+        }
+        return arr2;
+    }
 }
