1ee46028290393cc42cc271c356ec0186f11e448
waachaa <58870161+waachaa@users.noreply.github.com>
2021-09-26 17:52:49 -0400

    Update UnaryFunction.java

--- src/main/java/UnaryFunction.java
+++ src/main/java/UnaryFunction.java
@@ -1,3 +1,9 @@
 public interface UnaryFunction {
     double exec(double argument);
 }
+
+class Doubler implements UnaryFunction {
+    public double exec(double argument) {
+        return argument*2;
+    }
+}
